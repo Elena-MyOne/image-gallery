ac954ff25d9c16e1bd5f385bbb362d7ef1fe136c
Elena-MyOne <yakovenko.ea72@gmail.com>
2024-02-21 16:14:04 -0500

    feat: add uploading the image file functionality

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,11 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../Card/Card';
 import UploadForm from '../UploadForm/UploadForm';
+import { FileItem } from '../../models/interfaces';
 
 const photos: string[] = [
-  'https://picsum.photos/id/1001/200/200',
-  'https://picsum.photos/id/1002/200/200',
-  'https://picsum.photos/id/1003/200/200',
   'https://picsum.photos/id/1004/200/200',
   'https://picsum.photos/id/1005/200/200',
   'https://picsum.photos/id/1006/200/200',
@@ -14,16 +12,37 @@ const photos: string[] = [
 const MainPage: React.FC = () => {
   const [isCollapsed, setIsCollapsed] = useState(false);
   const [items, setItems] = useState<string[]>(photos);
-  const [input, setInput] = useState<string>('');
+  const [inputs, setInputs] = useState<FileItem>({ title: null, file: null, path: null });
+  const [message, setMessage] = useState<string>('');
 
-  const handleOnChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setInput(event.currentTarget.value);
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value, files } = event.target;
+
+    if (name === 'file' && files && files.length > 0) {
+      const selectedFile = files[0];
+
+      setInputs({
+        ...inputs,
+        file: selectedFile,
+        path: URL.createObjectURL(selectedFile),
+      });
+    } else {
+      setInputs({ ...inputs, title: value });
+    }
   };
 
   const handleOnSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    setItems([input, ...items]);
+    if (inputs.path) {
+      setItems([inputs.path, ...items]);
+    }
+    setInputs({ title: null, file: null, path: null });
+    setIsCollapsed(false);
   };
+
+  useEffect(() => {
+    setMessage(`I've add a new image ${inputs.title}`);
+  }, [inputs]);
 
   return (
     <section className='container text-center mt-5'>
@@ -31,7 +50,13 @@ const MainPage: React.FC = () => {
         {isCollapsed ? 'Close' : 'Add file'}
       </button>
       <div className='clearfix mb-4'></div>
-      <UploadForm isVisible={isCollapsed} onChange={handleOnChange} onSubmit={handleOnSubmit} />
+      {inputs.title && <p>{message}</p>}
+      <UploadForm
+        isVisible={isCollapsed}
+        onChange={handleOnChange}
+        onSubmit={handleOnSubmit}
+        inputs={inputs}
+      />
       <h1 className='mb-5'>Gallery</h1>
       <div className='cards row'>
         {items.map((photo, index) => (
--- src/components/UploadForm/UploadForm.tsx
+++ src/components/UploadForm/UploadForm.tsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { FileItem } from '../../models/interfaces';
+import Preview from './Preview/Preview';
 
 interface UploadFormProps {
   isVisible: boolean;
-  onChange: (event: React.FormEvent<HTMLInputElement>) => void;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+  inputs: FileItem;
 }
 
-const UploadForm: React.FC<UploadFormProps> = ({ isVisible, onChange, onSubmit }) => {
+const UploadForm: React.FC<UploadFormProps> = ({ isVisible, onChange, onSubmit, inputs }) => {
+  const isDisabled = useMemo(() => {
+    return !!Object.values(inputs).some((input) => !input);
+  }, [inputs]);
+
   return (
     <>
       {isVisible && (
         <>
           <p className='display-6 text-center mb-3'>Upload Stock Image</p>
           <div className='mb-5 d-flex align-items-center justify-content-center'>
+            <Preview {...inputs} />
             <form className='mb-2' style={{ textAlign: 'left' }} onSubmit={onSubmit}>
               <div className='mb-3'>
                 <input
@@ -27,7 +35,7 @@ const UploadForm: React.FC<UploadFormProps> = ({ isVisible, onChange, onSubmit }
               <div className='mb-3'>
                 <input type='file' className='form-control' name='file' onChange={onChange} />
               </div>
-              <button type='submit' className='btn btn-success float-end'>
+              <button type='submit' className='btn btn-success float-end' disabled={isDisabled}>
                 Save changes
               </button>
             </form>
